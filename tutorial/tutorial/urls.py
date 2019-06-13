5fcf9742d0b5da5d66b12fc705f9fd9da80a31f8
oikwsat <oikwsat@gmail.com>
2019-06-14 07:56:30 +0900

    Tutorial 2: Requests and Responses - Adding optional format suffixes to our URLs

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('snippets.urls')),
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>', views.snippet_detail),
 ]
